62774f8bf4fa928e237c9f051ba88756502ee929
Tammer <jeweisla@uci.edu>
2025-06-20 15:41:32 -0700

    Update activation functions module and registry. Add docstring.

--- act_fns.py
+++ act_fns.py
@@ -1,7 +1,25 @@
 """
-Docstring to be added later.
-This module constains act fn classes
+This module contains activation function classes and a registry to store them.
+Additional or custom activation functions can be registered by the user if desired, 
+either in this file or inline.
+A registry system is used to enable serialization by PyTorch Lightning's checkpointing 
+system and automatic hyperparameter saving, as class objects are not diretly 
+serializable.
+Example usage:
+    # Importing and using the registry and adding a custom activation function
+    from act_fns import ACT_FN_REGISTRY, register_act_fn
+
+    # Registering a custom activation function (inline or in this file)
+    @register_act_fn("CustomActFn")
+    class CustomActFn(nn.Module):
+        def forward(self, x):
+            return torch.sin(x)  # Example custom activation function
+To view inluded activation functions, use:
+    print(ACT_FN_REGISTRY.keys())
 """
+
+import torch
+from torch import nn
 
 # Activation function registry for storing different types of activation functions
 
@@ -21,3 +39,27 @@ def register_act_fn(name):
         ACT_FN_REGISTRY[name] = cls
         return cls
     return decorator
+
+@register_act_fn("Linear")
+class Linear(nn.Linear):
+    pass
+
+@register_act_fn("Tanh")
+class Tanh(nn.Tanh):
+    pass
+
+@register_act_fn("ReLU")
+class ReLU(nn.ReLU):
+    pass
+
+@register_act_fn("Sigmoid")
+class Sigmoid(nn.Sigmoid):
+    pass
+
+@register_act_fn("LeakyReLU")
+class LeakyReLU(nn.LeakyReLU):
+    pass
+
+@register_act_fn("Softmax")
+class Softmax(nn.Softmax):
+    pass
