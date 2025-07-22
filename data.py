13c341800d560b41e690a8c6792a2cd3bb2da267
Tammer <jeweisla@uci.edu>
2025-07-21 18:44:37 -0700

    add data.py for storing data generation / handling functions and classes.

---
+++ data.py
@@ -0,0 +1,7 @@
+"""
+data.py
+This module contains classes and functions for generating and handling datasets to be 
+used with ProNDF. User may add additional or custom datasets via files, which can then 
+be loaded.
+"""
+
